5ef1ef92735b0aafa67bfb381c239e1d7170ab33
Surya T N <suryatej.nim@yahoo.com>
2022-11-09 13:03:44 +0000

    Updated CSS styling of JSX elements in CartSummary component. Implemented JSX element tree to render the intended UI with cart summary details in CartSummary component.

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,1 +1,50 @@
-// Write your code here
+import CartContext from '../../context/CartContext'
+
+import './index.css'
+
+const CartSummary = () => {
+  const computeCartTotal = cartItemList => {
+    const cartTotal = 0
+
+    cartItemList.reduce((previousTotal, currentCartListItem) => {
+      const {price, quantity} = currentCartListItem
+      const currentCartItemTotalPrice = price * quantity
+
+      const updatedCartItemTotalPrice =
+        previousTotal + currentCartItemTotalPrice
+
+      return updatedCartItemTotalPrice
+    }, cartTotal)
+  }
+
+  return (
+    <div className="cart-summary-container">
+      <div className="cart-summary-details-container">
+        <CartContext.Consumer>
+          {cartContextDataObject => {
+            const {cartList} = cartContextDataObject
+
+            return (
+              <>
+                <h1 className="cart-order-total-text">
+                  Order Total:{' '}
+                  <span className="cart-order-total">
+                    Rs {computeCartTotal(cartList)}/-
+                  </span>
+                </h1>
+                <p className="cart-item-count-text">
+                  {cartList.length} Items in cart
+                </p>
+              </>
+            )
+          }}
+        </CartContext.Consumer>
+      </div>
+      <button type="button" className="cart-checkout-button">
+        Checkout
+      </button>
+    </div>
+  )
+}
+
+export default CartSummary
